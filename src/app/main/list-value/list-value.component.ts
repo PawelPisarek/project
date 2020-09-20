85dc84c84d48fbf3d92d602a4e3637c3e60eec57
Paweł Pisarek <pisarekp73@gmail.com>
2020-09-20 17:53:44 +0200

    annimation

--- src/app/main/list-value/list-value.component.ts
+++ src/app/main/list-value/list-value.component.ts
@@ -1,17 +1,22 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ValuePriceDataModel} from '../value-price-data.model';
+import { Component, Input, OnInit } from "@angular/core";
+import { ValuePriceDataModel } from "../value-price-data.model";
 
 @Component({
-  selector: 'app-list-value',
-  templateUrl: './list-value.component.html',
-  styleUrls: ['./list-value.component.scss']
+  selector: "app-list-value",
+  templateUrl: "./list-value.component.html",
+  styleUrls: ["./list-value.component.scss"],
 })
 export class ListValueComponent implements OnInit {
-
-  @Input() list: ValuePriceDataModel[];
-  constructor() { }
-
-  ngOnInit() {
+  show: boolean;
+  list: ValuePriceDataModel[];
+  @Input() set conversionList(value: ValuePriceDataModel[]) {
+    this.show = true;
+    this.list = value;
+    setTimeout(() => {
+      this.show = false;
+    }, 0);
   }
+  constructor() {}
 
+  ngOnInit() {}
 }
